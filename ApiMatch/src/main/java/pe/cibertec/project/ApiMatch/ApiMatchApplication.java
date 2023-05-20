9569d4af8185fb61d8b93073e967bb18090964a1
rasec1106 <elch.herrera@gmail.com>
2023-05-20 17:07:49 -0500

    Create ApiMatch

---
+++ ApiMatch/src/main/java/pe/cibertec/project/ApiMatch/ApiMatchApplication.java
@@ -0,0 +1,13 @@
+package pe.cibertec.project.ApiMatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMatchApplication.class, args);
+	}
+
+}
---
+++ ApiMatch/src/test/java/pe/cibertec/project/ApiMatch/ApiMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.cibertec.project.ApiMatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
