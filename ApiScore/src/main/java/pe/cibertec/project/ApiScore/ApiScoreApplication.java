3de5e9a7d469eb09b9a88e2e5dc780a45fb6609b
rasec1106 <elch.herrera@gmail.com>
2023-05-20 17:16:07 -0500

    Create ApiScore

---
+++ ApiScore/src/main/java/pe/cibertec/project/ApiScore/ApiScoreApplication.java
@@ -0,0 +1,13 @@
+package pe.cibertec.project.ApiScore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiScoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiScoreApplication.class, args);
+	}
+
+}
---
+++ ApiScore/src/test/java/pe/cibertec/project/ApiScore/ApiScoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.cibertec.project.ApiScore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiScoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
