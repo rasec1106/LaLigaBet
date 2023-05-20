e83dda3a49df5e5c1f2e286e77275784a65f2ec6
rasec1106 <elch.herrera@gmail.com>
2023-05-20 13:59:10 -0500

    Create ApiUser

---
+++ ApiUser/src/main/java/pe/cibertec/project/Api/ApiUserApplication.java
@@ -0,0 +1,13 @@
+package pe.cibertec.project.Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUserApplication.class, args);
+	}
+
+}
---
+++ ApiUser/src/test/java/pe/cibertec/project/Api/ApiUserApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.cibertec.project.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
