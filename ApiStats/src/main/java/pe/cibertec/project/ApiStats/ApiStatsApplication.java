cf12df4704e605533d765a5752e8ddfd1d142e22
rasec1106 <elch.herrera@gmail.com>
2023-05-20 17:20:52 -0500

    Create ApiStats

---
+++ ApiStats/src/main/java/pe/cibertec/project/ApiStats/ApiStatsApplication.java
@@ -0,0 +1,13 @@
+package pe.cibertec.project.ApiStats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiStatsApplication.class, args);
+	}
+
+}
---
+++ ApiStats/src/test/java/pe/cibertec/project/ApiStats/ApiStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.cibertec.project.ApiStats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
