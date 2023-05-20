c8f54b79e675cc671d277781df9254df03e5c087
rasec1106 <elch.herrera@gmail.com>
2023-05-20 17:04:46 -0500

    Create ApiPlayer

---
+++ ApiPlayer/src/main/java/pe/cibertec/project/ApiPlayer/ApiPlayerApplication.java
@@ -0,0 +1,13 @@
+package pe.cibertec.project.ApiPlayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPlayerApplication.class, args);
+	}
+
+}
---
+++ ApiPlayer/src/test/java/pe/cibertec/project/ApiPlayer/ApiPlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.cibertec.project.ApiPlayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPlayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
