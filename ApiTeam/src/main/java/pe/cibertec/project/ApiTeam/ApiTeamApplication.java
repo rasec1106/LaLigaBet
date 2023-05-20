b8055469a1a5cef8a524d1215d983da7fde60616
rasec1106 <elch.herrera@gmail.com>
2023-05-20 16:59:39 -0500

    Create ApiTeam

---
+++ ApiTeam/src/main/java/pe/cibertec/project/ApiTeam/ApiTeamApplication.java
@@ -0,0 +1,13 @@
+package pe.cibertec.project.ApiTeam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTeamApplication.class, args);
+	}
+
+}
---
+++ ApiTeam/src/test/java/pe/cibertec/project/ApiTeam/ApiTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.cibertec.project.ApiTeam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
