f69be7fe146c01293b085f3d3262d2db543ba85d
rasec1106 <elch.herrera@gmail.com>
2023-05-23 23:40:17 -0500

    Create entities

---
+++ ApiMatch/src/main/java/pe/cibertec/project/ApiMatch/Entity/Team.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.cibertec.project.ApiMatch.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.List;
+import lombok.Data;
+
+/**
+ *
+ * @author Cesar.Herrera
+ */
+@Entity
+@Data
+@Table(name = "teams")
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "country")
+    private String country;
+
+    @OneToMany(mappedBy = "team")
+    private List<Player> players;
+
+    // Other attributes and relationships
+    // Getters and setters
+}
