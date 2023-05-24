2ad17f4265867dbfb61826ac2f808e624ff0b87b
Leonardo Alain <alain.vg.96@gmail.com>
2023-05-24 07:52:04 -0500

    creacion de paquetes y 01 entidad

---
+++ ApiUser/src/main/java/pe/ciberte/project/ApiUser/entity/User.java
@@ -0,0 +1,21 @@
+package pe.ciberte.project.ApiUser.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String dni;
+    private String usuario;
+    private String correo;
+    private String contrasenia;
+    private Double saldo;
+}
