38aefc5d776ea74de1ac14947684535688d82cb9
FranDelgadillo <frandelgadilloso@gmail.com>
2023-06-26 01:16:45 -0500

    Francisco Final

--- ApiStats/src/main/java/pe/cibertec/bet/ApiStats/service/ApiRestClient.java
+++ ApiStats/src/main/java/pe/cibertec/bet/ApiStats/service/ApiRestClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import pe.cibertec.bet.ApiStats.entity.Player;
 
-@FeignClient(value="client-rest",url="http://localhost:8081/")
+@FeignClient(value="feign-client",url="http://apiplayer:8081/")
 public interface ApiRestClient {
     
     @GetMapping("/api/player/findByIdPlayer/{idPlayer}")
